32ac753f4774613e464cd89ad6fd7854c17b5dc3
Dhaivat Dave <48772054+dhaivat7@users.noreply.github.com>
2020-09-25 23:41:01 -0700

    Create counter.sv

---
+++ counter.sv
@@ -0,0 +1,24 @@
+module counter  (
+                output logic [4:0] count,
+                input  logic [4:0] data ,
+                input        clk  ,
+                input        load ,
+                input        enable ,
+                input        rst_   
+                );
+
+// SystemVerilog: time unit and time precision declaration
+timeunit 1ns;
+timeprecision 100ps;
+
+// SystemVerilog: always_ff 
+always_ff @(posedge clk, negedge rst_)
+   if (!rst_)
+       count <= 0;
+   else if (load)
+       count <= data;
+   else if (enable)
+// SystemVerilog: postincrement or assignment operator
+       count <= count +1;  //++;
+
+endmodule
